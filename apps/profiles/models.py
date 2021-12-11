0e4d77e9c2b7aa7e76c39d3ae61e7044b752d071
jvanderlaan95 <j.vanderlaan@hij5.nl>
2021-12-11 13:36:58 +0100

    ERD + user models

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Role(models.Model):
+    """
+    Role is een functie die een dude bekleed binnen het dispuut
+    """
+    name = models.CharField(max_length=30)
+
+
+class Profile(models.Model):
+    """
+    Profile is een extended user account. Elke dude heeft een profile.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, verbose_name="Functie", null=True, blank=True)
+    lichting = models.IntegerField(verbose_name="Lichting")
+    first_name = models.CharField(max_length=50, verbose_name="Voornaam")
+    last_name = models.CharField(max_length=50, verbose_name="Achternaam")
+    dude_name = models.CharField(max_length=50, verbose_name="Dude naam")
+    avatar = models.ImageField(verbose_name="Avatar")
+    description = models.TextField(verbose_name="Description")
--- funsport/settings.py
+++ funsport/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.profiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
