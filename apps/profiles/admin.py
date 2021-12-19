93bbd5f405ad413955ee991d82ca0f2d273bfce9
jvanderlaan95 <j.vanderlaan@hij5.nl>
2021-12-19 22:56:46 +0100

    Lichting field verwijderd, current role is nu group relatie

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from apps.profiles.models import Role
 from .models import User
 
 
-
 # Register your models here.
-admin.site.register(Role)
 admin.site.register(User)
--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,30 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-
-class Role(models.Model):
-    """
-    Role is een functie die een dude bekleed binnen het dispuut
-    """
-    name = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.name
-
+from django.contrib.auth.models import AbstractUser, Group
 
 class User(AbstractUser):
     """
     Profile is een extended user account. Elke dude heeft een profile.
     """
-    role = models.ForeignKey(Role, on_delete=models.SET_NULL, verbose_name="Functie", null=True, blank=True)
-    lichting = models.IntegerField(verbose_name="Lichting", null=True, blank=True)
     first_name = models.CharField(max_length=50, verbose_name="Voornaam", null=True, blank=True)
     last_name = models.CharField(max_length=50, verbose_name="Achternaam", null=True, blank=True)
     dude_name = models.CharField(max_length=50, verbose_name="Dude naam", null=True, unique=True, blank=True)
     avatar = models.ImageField(verbose_name="Avatar", null=True, blank=True)
     description = models.TextField(verbose_name="Description", null=True, blank=True)
+    current_role = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, blank=False, related_name="active_dude")
 
     def __str__(self):
         return self.username
